32e7f9af112cfde6ccee0bd1923965eec1e6d070
rabbice <iankater@gmail.com>
2021-02-04 13:33:19 +0000

    print numbers with commas and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num == 9)
+		{
+			num++;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
