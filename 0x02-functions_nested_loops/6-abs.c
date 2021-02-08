99d5287213ef82e091fdcb0af24a5fcba1eea3ab
rabbice <iankater@gmail.com>
2021-02-08 11:52:03 +0000

    print absolute value of numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _abs - return absolute value
+ * @y: int checked
+ * Return: absolute value
+ */
+int _abs(int y)
+{
+	if (y < 0)
+	{
+		return(0 - y);
+	}
+	else
+	{
+		return(y);
+	}
+}
