0346721750943417f33cf8b3b2d9aaa87a19f77c
rabbice <iankater@gmail.com>
2021-02-11 12:12:31 +0000

    fizz buzz algo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * print 1 - 100 except multiple of 3 & 5
- * Return: Fizz || Buzz || FizzBuzz
+ *main - print numbers 1 to 100
+ *
+ *Return: print to stdout numbers 1 to 100 except:
+ *replace multiples of three print Fizz
+ *replace multiples of five print Buzz
+ *numbers which are multiples of both three and five print FizzBuzz
  */
-
 int main(void)
 {
-	int x;
+	int a;
 
-	for (x = 1; x <= 100; x++)
+	for (a = 1; a <= 100; a++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if ((a % 3) == 0 && (a % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		if (x % 3 == 0)
+		else if ((a % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (x % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (x == 100)
+		else if (a == 100)
 		{
 			printf("Buzz");
 		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz ");
+		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d ", a);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
