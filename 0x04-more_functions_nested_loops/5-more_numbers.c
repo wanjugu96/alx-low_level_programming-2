62972cfc8b5b826a59c63ef18bf22d29ef3977d7
rabbice <iankater@gmail.com>
2021-02-11 09:57:30 +0000

    print 0-14 10x function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print 0 - 14 x10
+ * Return: 0 - 14 x10
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
