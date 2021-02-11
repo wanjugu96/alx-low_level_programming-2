25890686593bdfa558d16dbff11e0a2b76667f79
rabbice <iankater@gmail.com>
2021-02-11 10:26:51 +0000

    straight line function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_line - print straight line
+ * @n: int check
+ * Return: straight line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
