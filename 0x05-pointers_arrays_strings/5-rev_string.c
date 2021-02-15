062c3f95b0d9f8ec06b0430cbfa72a994be6d47b
rabbice <iankater@gmail.com>
2021-02-15 15:22:46 +0000

    reverse string algorithm

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * rev_string - stacks algo
+ * @s: string value
+ * Return: the characters
+ */
+
+void rev_string(char *s)
+{
+	int i, j, k, length;
+	char *begin, *end, tmp;
+	
+	/* check for length of string and store in i */
+	length = 0;
+	for (i = 0; s[i] != 0; i++)
+	{
+		length++;
+	}
+	/* assign pointers to string value */
+	begin = s;
+	end = s;
+	
+	/* reverse the string */
+	for (j = 0; j < length - 1; j++)
+		end++;
+
+	for (k = 0; k < length / 2; k++)
+	{
+		tmp = *end;
+		*end = *begin;
+		*begin = tmp;
+		begin++;
+		end--;
+	}
+}
