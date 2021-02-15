cf825ea859659a0b890e059af8d0f47dbefdfb98
rabbice <iankater@gmail.com>
2021-02-15 15:23:45 +0000

    reverse string algorithm

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	int i, j, k, length;
 	char *begin, *end, tmp;
-	
+
 	/* check for length of string and store in i */
 	length = 0;
 	for (i = 0; s[i] != 0; i++)
@@ -20,7 +20,7 @@ void rev_string(char *s)
 	/* assign pointers to string value */
 	begin = s;
 	end = s;
-	
+
 	/* reverse the string */
 	for (j = 0; j < length - 1; j++)
 		end++;
