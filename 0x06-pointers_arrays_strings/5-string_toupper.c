8fd942b517fd5fb88891415e785c9bcc3d63b8c1
rabbice <iankater@gmail.com>
2021-02-18 10:27:49 +0000

    convert lowercase to uppercase in string function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - convert lowercase to uppercase
+ * @s: string
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
