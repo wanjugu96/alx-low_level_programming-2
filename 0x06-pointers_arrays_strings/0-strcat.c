b98b857c9a1bed1fa5588d1af97ab8cbebd5aa7d
rabbice <iankater@gmail.com>
2021-02-18 09:08:31 +0000

    added parentheses

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -23,5 +23,5 @@ char *_strcat(char *dest, char *src)
 		ptr++;
 	}
 	*ptr = '\0';
-	return dest;
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,5 +23,5 @@ char *_strncat(char *dest, char *src, int n)
 		ptr++;
 	}
 	*ptr = '\0';
-	return dest;
+	return (dest);
 }
