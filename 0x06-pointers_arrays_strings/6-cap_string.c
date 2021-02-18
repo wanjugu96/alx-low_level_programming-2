4c88dc4d3a8356b7796416721d8b10242ef6cdd2
rabbice <iankater@gmail.com>
2021-02-18 11:06:14 +0000

    capitalize first letter of word in string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - capitalize first letters of word
+ * @c: string
+ * Return: uppercase word in string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i - 1] == '\t' || s[i - 1] == ' ' || s[i - 1] == '\n')
+				s[i] -= 32;
+			else if (s[i - 1] == ',')
+				s[i] -= 32;
+			else if (s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!')
+				s[i] -= 32;
+			else if (s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(')
+				s[i] -= 32;
+			else if (s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+				s[i] -= 32;
+		}
+	}
+	return (s);
+}
