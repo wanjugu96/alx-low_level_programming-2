accacd5e04795c753659c0b996e6b3bff64da7e0
rabbice <iankater@gmail.com>
2021-02-18 11:25:49 +0000

    updated to ascii type

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
-
 /**
- * *cap_string - capitalize first letters of word
+ * *cap_string - convert lower to upper, first letter of a word
  * @s: string
- * Return: uppercase word
+ * Return: uppercase word string
  */
-
 char *cap_string(char *s)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			if (s[i - 1] == '\t' || s[i - 1] == ' ' || s[i - 1] == '\n')
 				s[i] = s[i] - 32;
