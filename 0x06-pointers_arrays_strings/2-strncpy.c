06c3d38d9f65fe67f2619e1c23312218b2d546c4
rabbice <iankater@gmail.com>
2021-02-18 09:17:07 +0000

    copy string function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _strncopy - copy string to dest in n length
+ * *_strncpy - copy string to dest in n length
  * @dest: string to return
  * @src:  '' '' copy
  * @n: size to copy
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 		*ptr = src[y];
 		ptr++;
 	}
-	for (;y < n; y++)
+	for (; y < n; y++)
 	{
 		*ptr = '\0';
 		ptr++;
