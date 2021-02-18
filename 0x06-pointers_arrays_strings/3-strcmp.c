e89d6cdac2de1c2e2bd0d5c3ef2aafd6e359f9c5
rabbice <iankater@gmail.com>
2021-02-18 09:44:58 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,10 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-		{
 			return (s1[i] - s2[i]);
-		}
 		else if (s1[i] < s2[i])
-		{
 			return (-(s2[i] - s1[i]));
-			i++;
-		}
-		return (0);
+		i++;
 	}
+	return (0);
 }
