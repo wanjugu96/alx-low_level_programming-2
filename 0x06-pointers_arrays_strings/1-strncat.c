5d79ec399fc67a808065220c4e578bfdfebb8b00
rabbice <iankater@gmail.com>
2021-02-18 08:40:04 +0000

    concatenate two strings without null termination

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat - append src to dest
+ * @dest: return
+ * @src: append
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int j, k;
+	char *ptr;
+
+	ptr = dest;
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+		ptr++;
+	}
+	for (k = 0; src[k] != '\0' && k < n; k++)
+	{
+		*ptr = src[k];
+		ptr++;
+	}
+	*ptr = '\0';
+	return dest;
+}
