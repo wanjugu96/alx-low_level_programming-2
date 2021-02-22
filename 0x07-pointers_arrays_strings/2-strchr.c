97789c836e3d96d0ffd14fa8c6f3f31fd3131604
rabbice <iankater@gmail.com>
2021-02-22 10:03:12 +0000

    find char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - located char in string
+ * @s: string to check
+ * @c: char to check for
+ * Return: pointer to spot in s with c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (0);
+}
